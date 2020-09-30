27c75a676ec42de3b093b762fd9c900c776d03d8
Paul Brissaud <pbrissaud.pro@gmail.com>
2020-09-30 13:06:11 +0200

    Export InfuxDB ok

--- script.py
+++ script.py
@@ -1,13 +1,28 @@
 import yfinance as yf
 import json
 import time
+from influxdb import InfluxDBClient
+import datetime
 
 while True:
+    client = InfluxDBClient(host="localhost", port=8086, database="bourse")
     with open('data.json') as data_file:
         data = json.load(data_file)
         for action in data:
             ticker = yf.Ticker(action['sigle'])
             history = ticker.history()
             last_quote = (history.tail(1)['Close'].iloc[0])
-            print(action, last_quote)
-    time.sleep(10)
+            json_body = [
+                {
+                    "measurement": "quotations",
+                    "tags": {
+                        "nom": action['nom'],
+                        "sector": ticker.info['sector']
+                    },
+                    "fields": {
+                        "price": last_quote
+                    }
+                }
+            ]
+            client.write_points(json_body)
+            time.sleep(10)
