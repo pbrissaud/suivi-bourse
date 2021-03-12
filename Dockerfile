f21a5b3697e74c9ba2181ded8ea226505b4e9938
Paul <pbrissaud.pro@gmail.com>
2021-03-12 10:24:09 +0100

    feat: app parameters configuration
    Multiple changes on application customisation with paramaters:
    * The app now uses os.getnv() function to set up default values
    * The parameters can be now be passed as CLI arg (the CLi args overrides environnement variable)
    * Add a new parameter 'appScrapingInterval' to set up the scraping interval of the application
    * Add a new parameter 'appDataFilePath' to set up the path of the data file
    * Docker-compose read all env from .env file and set-up app, influx and grafana env variables
    
    With these changes, the application is designed to be run standalone. It can support for example influxDB authentification.

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,6 @@ COPY ./app/ .
 RUN python3 -m pip install influxdb
 RUN python3 -m pip install yfinance
 
-ENV INFLUXDB_HOST=suivi-bourse-influxdb
-ENV INFLUXDB_PORT=8086
-ENV INFLUXDB_DATABASE=bourse
-
 VOLUME [ "/data" ]
 
 CMD ["python3", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,19 +1,57 @@
 import json
 import os
 import time
+import sys
+import getopt
 import yfinance as yf
 from influxdb import InfluxDBClient
 
 
 class SuiviBourse:
-    def __init__(self):
-        self.influxHost = os.environ['INFLUXDB_HOST']
-        self.influxPort = os.environ['INFLUXDB_PORT']
-        self.influxDatabase = os.environ['INFLUXDB_DATABASE']
+    def __init__(self, argv):
+        try:
+            opts, _ = getopt.getopt(
+                argv, "hH:P:D:U:P:i:p:", ["help", "host=", "port=", "database=", "username=", "password=", "interval=", "path="]
+            )
+        except getopt.GetoptError as err:
+            print(err)
+            usage()
+            sys.exit(1)
+        
+        influxHost = os.getenv('INFLUXDB_HOST', default='localhost')
+        influxPort = os.getenv('INFLUXDB_PORT', default=8086)
+        influxDatabase = os.getenv('INFLUXDB_DATABASE', default='bourse')
+        influxUsername = ""
+        influxPassword = ""
+
+        self.appScrapingInterval = os.getenv('APP_SCRAPING_INTERVAL', default=60)
+        self.appDataFilePath = os.getenv('APP_FILE_PATH', default='/data/data.json')
+
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                usage()
+                sys.exit(0)
+            elif opt in ("-H", "--host"):
+                influxHost = arg
+            elif opt in ("-P", "--port"):
+                influxPort = arg
+            elif opt in ("-D", "--database"):
+                influxDatabase = arg
+            elif opt in ("-U", "--username"):
+                influxUsername = arg
+            elif opt in ("-P", "--password"):
+                influxPassword = arg
+            elif opt in ("-i", "--interval"):
+                self.appScrapingInterval = arg
+            elif opt in ("-p", "--path"):
+                self.appDataFilePath = arg
+
+        self.influxdbClient = InfluxDBClient(host=influxHost,port=influxPort,database=influxDatabase,username=influxUsername,password=influxPassword)
+        
 
     def run(self):
         try:
-            with open('/data/data.json') as data_file:
+            with open(self.appDataFilePath) as data_file:
                 data = json.load(data_file)
                 for action in data:
                     ticker = yf.Ticker(action['sigle'])
@@ -38,20 +76,21 @@ class SuiviBourse:
                             action['patrimoine']['prix_revient']
                         }
                     }]
-                    influxdbClient = InfluxDBClient(
-                        host=self.influxHost,
-                        port=self.influxPort,
-                        database=self.influxDatabase)
-                    influxdbClient.write_points(json_body)
-                    influxdbClient.close()
+                    self.influxdbClient.write_points(json_body)
+                    self.influxdbClient.close()
         except Exception as e:
             print(e)
+            sys.exit(1)
+
+def usage():
+    print("usage")
+        
 
 
 if __name__ == "__main__":
-    suivi = SuiviBourse()
+    suivi = SuiviBourse(sys.argv[1:])
     # If you want to run the app not in Docker, replace next lines with 'suivi.run()' 
     while True:
         suivi.run()
-        time.sleep(60)
+        time.sleep(suivi.appScrapingInterval)
 
