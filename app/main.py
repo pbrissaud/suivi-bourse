18edae4e2d6f5d0d09ca910a005f91dc9af95308
Paul Brissaud <pbrissaud.pro@gmail.com>
2021-05-30 00:43:28 +0200

    feat: update app for influx2 communication

--- app/main.py
+++ app/main.py
@@ -28,14 +28,16 @@ class SuiviBourse:
 
         influxHost = os.getenv('INFLUXDB_HOST', default='localhost')
         influxPort = os.getenv('INFLUXDB_PORT', default=8086)
-        influxDatabase = os.getenv('INFLUXDB_DATABASE', default='bourse')
-        influxUsername = ""
-        influxPassword = ""
+        influxToken = os.getenv('INFLUXDB_TOKEN', default='')
+        influxProtocol = os.getenv('INFLUXDB_PROTOCOL', default='http')
 
-        self.appScrapingInterval = int(
-            os.getenv('APP_SCRAPING_INTERVAL', default=60))
-        self.appDataFilePath = os.getenv(
-            'APP_FILE_PATH', default='/data/data.json')
+        if influxProtocol not in ['http', 'https']:
+            logging.error("Le protocol ")
+
+        self.influxBucket = os.getenv('INFLUXDB_BUCKET', default='bourse')
+        self.influxOrg =  os.getenv('INFLUXDB_ORG', default='')
+        self.appScrapingInterval = int(os.getenv('APP_SCRAPING_INTERVAL', default=60))
+        self.appDataFilePath = os.getenv('APP_FILE_PATH', default='/data/data.json')
 
         for opt, arg in opts:
             if opt in ("-h", "--help"):
@@ -56,15 +58,13 @@ class SuiviBourse:
             elif opt in ("-c", "--config"):
                 self.appDataFilePath = arg
 
-        self.influxdbClient = InfluxDBClient(
-            host=influxHost, port=influxPort, database=influxDatabase,
-            username=influxUsername, password=influxPassword)
+        self.influxdbClient: InfluxDBClient = InfluxDBClient(url=influxProtocol+"://"+influxHost+":"+influxPort,token=influxToken)
 
         with open(Path(__file__).parent / "schema.yaml") as f:
             self.dataSchema = yaml.load(f, Loader=yaml.FullLoader)
 
     def check(self):
-        self.influxdbClient.ping()
+        self.influxdbClient.ready()
         if(not os.path.exists(self.appDataFilePath)):
             raise Exception(
                 "Data file {} doesn't exist !".format(self.appDataFilePath))
@@ -116,8 +116,8 @@ class SuiviBourse:
                             }
                         }
                     ]
-                    self.influxdbClient.write_points(json_body)
-                    self.influxdbClient.close()
+                    write_api = self.influxdbClient.write_api(write_options=SYNCHRONOUS)
+                    write_api.write(self.influxBucket, self.influxOrg, json_body)
             else:
                 raise Exception("Data file isn't valid : {}".format(validation_error))
 
