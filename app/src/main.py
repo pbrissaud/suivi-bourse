1346ed3d6034b3891081579c3330e5fda9235d80
Paul Brissaud <pbrissaud.pro@gmail.com>
2022-08-16 23:42:13 +0200

    Revert "Release 3.2  (#48)"
    This reverts commit 341885ea989c27f273ee8128528ea827a1ff4a0a.

--- app/src/main.py
+++ app/src/main.py
@@ -11,119 +11,108 @@ import yaml
 import yfinance as yf
 from apscheduler.schedulers.blocking import BlockingScheduler
 from cerberus import Validator
-from confuse import Configuration
+from confuse import Configuration, exceptions as c_exceptions
 from logfmt_logger import getLogger
 from urllib3 import exceptions as u_exceptions
 
-app_logger = getLogger(
-    "suivi_bourse", level=os.getenv('LOG_LEVEL', default='INFO'))
-scheduler_logger = getLogger(
-    "apscheduler.scheduler", level=os.getenv('LOG_LEVEL', default='INFO'))
+logger = getLogger("suivi_bourse")
+
+sb_share_price = prometheus_client.Gauge(
+    "sb_share_price",
+    "Price of the share",
+    ["share_name", "share_symbol"]
+)
+
+sb_purchased_quantity = prometheus_client.Gauge(
+    "sb_purchased_quantity",
+    "Quantity of purchased share",
+    ["share_name", "share_symbol"]
+)
+
+sb_purchased_price = prometheus_client.Gauge(
+    "sb_purchased_price",
+    "Price of purchased share",
+    ["share_name", "share_symbol"]
+)
+
+sb_purchased_fee = prometheus_client.Gauge(
+    "sb_purchased_fee",
+    "Fees",
+    ["share_name", "share_symbol"]
+)
+
+sb_owned_quantity = prometheus_client.Gauge(
+    "sb_owned_quantity",
+    "Owned quantity of the share",
+    ["share_name", "share_symbol"]
+)
+
+sb_received_dividend = prometheus_client.Gauge(
+    "sb_received_dividend",
+    "Sum of received dividend for the share",
+    ["share_name", "share_symbol"]
+)
 
 
-class InvalidConfigFile(Exception):
-    def __init__(self, errors_):
-        self.errors = errors_
-        self.message = 'Shares field of the config file is invalid :' + \
-            str(self.errors)
-        super().__init__(self.message)
+def validate(validator: Validator, configuration: Configuration) -> bool:
+    """Validate config file wirth a validator
+
+    :param validator: Validator used to verify configuration
+    :param configuration: Config object
+    :return: Result of validation
+    :rtype: bool
+    """
+    return validator.validate({"shares": configuration['shares'].get()})
 
 
-class SuiviBourseMetrics:
-    def __init__(self, configuration_: Configuration, validator_: Validator):
-        self.configuration = configuration_
-        self.validator = validator_
-        self.init_metrics()
+def expose_metrics(configuration: Configuration):
+    """Refresh exposed data
 
-    def init_metrics(self):
-        if self.validate():
-            common_labels = ['share_name', 'share_symbol']
+    :param configuration: Config object
+    """
+    for share in configuration['shares'].get():
+        sb_purchased_quantity.labels(share_name=share['name'], share_symbol=share['symbol']).set(
+            share['purchase']['quantity'])
+        sb_purchased_price.labels(share_name=share['name'], share_symbol=share['symbol']).set(
+            share['purchase']['cost_price'])
+        sb_purchased_fee.labels(share_name=share['name'], share_symbol=share['symbol']).set(
+            share['purchase']['fee'])
+        sb_owned_quantity.labels(share_name=share['name'], share_symbol=share['symbol']).set(
+            share['estate']['quantity'])
+        sb_received_dividend.labels(share_name=share['name'], share_symbol=share['symbol']).set(
+            share['estate']['received_dividend'])
 
-            self.sb_share_price = prometheus_client.Gauge(
-                "sb_share_price",
-                "Price of the share",
-                common_labels
-            )
+        try:
+            ticker = yf.Ticker(share['symbol'])
+            history = ticker.history()
+            last_quote = (history.tail(1)['Close'].iloc[0])
+            sb_share_price.labels(
+                share_name=share['name'], share_symbol=share['symbol']).set(last_quote)
+        except u_exceptions.NewConnectionError as connection_exception:
+            logger.error(
+                "Error while retrieving data from Yfinance API : %s", connection_exception)
+        except RuntimeError as runtime_exception:
+            logger.error(
+                "Error while retrieving data from Yfinance API : %s", runtime_exception)
 
-            self.sb_purchased_quantity = prometheus_client.Gauge(
-                "sb_purchased_quantity",
-                "Quantity of purchased share",
-                common_labels
-            )
 
-            self.sb_purchased_price = prometheus_client.Gauge(
-                "sb_purchased_price",
-                "Price of purchased share",
-                common_labels
-            )
+def run(validator: Validator, configuration: Configuration):
+    """Validate config file and refresh metrics
 
-            self.sb_purchased_fee = prometheus_client.Gauge(
-                "sb_purchased_fee",
-                "Fees",
-                common_labels
-            )
+    :param validator: Validator used to verify configuration
+    :param configuration: Config object
+    """
+    configuration.reload()
 
-            self.sb_owned_quantity = prometheus_client.Gauge(
-                "sb_owned_quantity",
-                "Owned quantity of the share",
-                common_labels
-            )
+    if not validate(validator, configuration):
+        logger.error('Shares field of the config file is invalid : %s', validator.errors)
+        return
 
-            self.sb_received_dividend = prometheus_client.Gauge(
-                "sb_received_dividend",
-                "Sum of received dividend for the share",
-                common_labels
-            )
-
-            self.sb_share_info = prometheus_client.Gauge(
-                "sb_share_info",
-                "Share informations as label",
-                common_labels + ['share_currency', 'share_exchange',
-                                 'company_logo', 'share_market', 'company_sector']
-            )
-        else:
-            raise InvalidConfigFile(self.validator.errors)
-
-    def validate(self):
-        return self.validator.validate({"shares": self.configuration['shares'].get()})
-
-    def expose_metrics(self):
-        for share in self.configuration['shares'].get():
-            label_values = [share['name'], share['symbol']]
-
-            self.sb_purchased_quantity.labels(
-                *label_values).set(share['purchase']['quantity'])
-            self.sb_purchased_price.labels(
-                *label_values).set(share['purchase']['cost_price'])
-            self.sb_purchased_fee.labels(
-                *label_values).set(share['purchase']['fee'])
-            self.sb_owned_quantity.labels(
-                *label_values).set(share['estate']['quantity'])
-            self.sb_received_dividend.labels(
-                *label_values).set(share['estate']['received_dividend'])
-
-            try:
-                ticker_info = yf.Ticker(share['symbol']).info
-                last_quote = ticker_info['currentPrice']
-                self.sb_share_price.labels(*label_values).set(last_quote)
-                info_values = label_values + [ticker_info['currency'], ticker_info['exchange'],
-                                              ticker_info['logo_url'], ticker_info['market'], ticker_info['sector']]
-                self.sb_share_info.labels(*info_values).set(1)
-            except (u_exceptions.NewConnectionError, RuntimeError):
-                app_logger.error(
-                    "Error while retrieving data from Yfinance API", exc_info=True)
-
-    def run(self):
-        self.configuration.reload()
-
-        if not self.validate():
-            raise InvalidConfigFile(self.validator.errors)
-
-        self.expose_metrics()
+    expose_metrics(configuration)
 
 
 if __name__ == "__main__":
-    app_logger.info('SuiviBourse is running !')
+    logger.info('SuiviBourse is running !')
 
     # Load config
     config = Configuration('SuiviBourse', __name__)
@@ -135,17 +124,18 @@ if __name__ == "__main__":
 
     try:
         # Start up the server to expose the metrics.
-        prometheus_client.start_http_server(
-            int(os.getenv('SB_METRICS_PORT', default='8081')))
-        # Init SuiviBourseMetrics
-        sb_metrics = SuiviBourseMetrics(config, shares_validator)
+        prometheus_client.start_http_server(int(os.getenv('SB_METRICS_PORT', default='8081')))
         # Schedule run the job on startup.
-        sb_metrics.run()
+        run(shares_validator, config)
         # Start scheduler
         scheduler = BlockingScheduler()
-        scheduler.add_job(sb_metrics.run, 'interval', seconds=int(
+        scheduler.add_job(run, 'interval', args=[shares_validator, config], seconds=int(
             os.getenv('SB_SCRAPING_INTERVAL', default='120')))
         scheduler.start()
-    except Exception:
-        app_logger.critical('An error occurred', exc_info=True)
+    except c_exceptions.NotFoundError as confuse_exception_notfound:
+        logger.critical(
+            'Config file unreadable or non-existing field : %s', confuse_exception_notfound)
+        sys.exit(1)
+    except c_exceptions.ConfigTypeError as confuse_exception_configtype:
+        logger.critical('Field in config file is invalid : %s', confuse_exception_configtype)
         sys.exit(1)
