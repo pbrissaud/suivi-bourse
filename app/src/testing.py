78ad53ba22625c5fe699c7f2a59068b2cd262212
Paul Brissaud <pbrissaud.pro@gmail.com>
2022-11-04 16:29:37 +0100

    Better testing procedure
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

---
+++ app/src/testing.py
@@ -0,0 +1,46 @@
+from prometheus_client.parser import text_string_to_metric_families
+import requests
+import sys
+import prometheus_client
+import yaml
+import os
+from cerberus import Validator
+from confuse import Configuration
+from pathlib import Path
+
+from main import SuiviBourseMetrics
+
+# Load config
+config = Configuration('SuiviBourse', __name__)
+
+# Load schema file
+with open(Path(__file__).parent / "schema.yaml", encoding='UTF-8') as f:
+    dataSchema = yaml.safe_load(f)
+shares_validator = Validator(dataSchema)
+
+try:
+    # Start up the server to expose the metrics.
+    prometheus_client.start_http_server(
+        int(os.getenv('SB_METRICS_PORT', default='8081')))
+    # Init SuiviBourseMetrics
+    sb_metrics = SuiviBourseMetrics(config, shares_validator)
+    # Schedule run the job on startup.
+    sb_metrics.run()
+except Exception:
+    print('Program failed to start')
+    sys.exit(1)
+
+r = requests.get('http://localhost:8081/metrics')
+
+if r.status_code != 200:
+    print('Not received 200 response code')
+    sys.exit(1)
+
+metrics = r.text
+
+for family in text_string_to_metric_families(metrics):
+    for sample in family.samples:
+        if sample.name.startswith('sb_'):
+            print(sample.name + ' => ' + str(sample.value))
+            if sample.value == None:
+                sys.exit(1)
