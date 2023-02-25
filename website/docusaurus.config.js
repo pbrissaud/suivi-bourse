70e37e2baaebe669014c733851bc038e48d7fb2f
Paul Brissaud <pbrissaud.pro@gmail.com>
2023-02-25 02:13:23 +0100

    fix edit url
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            `https://github.com/${organizationName}/${projectName}/tree/main/`,
+            `https://github.com/${organizationName}/${projectName}/tree/master/website/`,
         },
         blog: false,
         theme: {
